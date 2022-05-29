09ab88c5a9097fa21f404f8d4e38583ef962097f
onumengine <onumdev@gmail.com>
2022-05-29 19:34:25 +0100

    Initial commit.

---
+++ src/chipspace_assets/src/index.js
@@ -0,0 +1,19 @@
+import { chipspace } from "../../declarations/chipspace";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await chipspace.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
